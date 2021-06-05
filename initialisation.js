3889aebe0ddc7a35bda97cb562527b4604566658
Michael Wirth <mwirth@shiftphones.com>
2021-06-05 21:23:31 +0200

    update 5

--- initialisation.js
+++ initialisation.js
@@ -1,5 +1,5 @@
-import { PlayerView } from '../components/player/class/playerView.js';
-import { PlayerModel } from '../components/player/class/playerModel.js';
-import { PlayerController } from '../components/player/class/playerController.js';
+import { PlayerView } from './components/player/class/playerView.js';
+import { PlayerModel } from './components/player/class/playerModel.js';
+import { PlayerController } from './components/player/class/playerController.js';
 
 const player = new PlayerController(new PlayerView(), new PlayerModel());
