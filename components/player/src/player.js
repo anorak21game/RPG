e2cc1e91b0d9a2b56fc3d78d63ff7622b16db1a4
Michael Wirth <mwirth@shiftphones.com>
2021-06-17 22:51:23 +0200

    changes

--- components/player/src/player.js
+++ components/player/src/player.js
@@ -1,53 +1,38 @@
 import { Events } from '../../../library/events.js';
 
-class Status {
+class Player {
   constructor() {
-    this.values = {
+    this.status = {
       player: 'components/player/asset/lars.png',
       walk: 'player__stop--down',
     }
-  }
 
-  getPlayer()           { return this.values.player; }
-  getWalk()             { return this.values.walk; }
-
-  setPlayer(value)      { this.values.player = value; }
-  setWalk(value)        { this.values.player = value; }
-}
-
-class Body {
-  constructor() {
     this.player = document.getElementById("player");
     this.player.className = "player";
     this.image = document.createElement("img");
     this.player.append(this.image);
   }
 
-  setSrc(value)         { this.image.src = value; }
-  setClassName(value)   { this.image.className = value; }
-
-  // selectPlayer() {
-  //   document.addEventListener('keypress', (event) => {
-  //     event.preventDefault();
-  //     if (event.key == '1') this.image.src = 'components/player/asset/lars.png';
-  //     if (event.key == '2') this.image.src = 'components/player/asset/levi.png';
-  //     if (event.key == '3') this.image.src = 'components/player/asset/lenny.png';
-  //   })
-  // }
-}
-
-class Connector extends Events {
-  constructor(body, status) {
-    super();
-    this.body = body;
-    this.status = status;
-
-    this.body.setSrc(this.status.getPlayer());
-    this.body.setClassName(this.status.getWalk());
+  selectPlayer() {
+    document.addEventListener('keypress', (event) => {
+      event.preventDefault();
+      if (event.key == '1') this.image.src = 'components/player/asset/lars.png';
+      if (event.key == '2') this.image.src = 'components/player/asset/levi.png';
+      if (event.key == '3') this.image.src = 'components/player/asset/lenny.png';
+    })
   }
 }
 
-const player = new Connector(new Body(), new Status());
+class Execute extends Events {
+  constructor(player) {
+    super();
+    this.player = player;
+
+    this.player.selectPlayer();
+  }
+}
+
+const player = new Execute(new Player);
 console.log(player);
 
 
