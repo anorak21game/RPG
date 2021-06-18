8b6de4e899245a5baf40342b5a2a7addf513745b
Michael Wirth <mwirth@shiftphones.com>
2021-06-18 22:01:33 +0200

    clean folders

--- src/player.js
+++ src/player.js
@@ -5,17 +5,10 @@ player.className = "player";
 let image = document.createElement("img");
 player.append(image);
 
-image.src = 'components/player/asset/lars.png';
+image.src = 'asset/lars.png';
 image.className = 'player__stop--down';
 
 /* add events */
-
-document.addEventListener('keypress', (event) => {
-  event.preventDefault();
-  if (event.key == '1') image.src = 'components/player/asset/lars.png';
-  if (event.key == '2') image.src = 'components/player/asset/levi.png';
-  if (event.key == '3') image.src = 'components/player/asset/lenny.png';
-});
 
 document.addEventListener('keydown', (event) => {
   event.preventDefault();
@@ -23,6 +16,10 @@ document.addEventListener('keydown', (event) => {
   if (event.key == 'ArrowDown') image.className = 'player__walk--down';
   if (event.key == 'ArrowRight') image.className = 'player__walk--right';
   if (event.key == 'ArrowLeft') image.className = 'player__walk--left';
+
+  if (event.key == '1') image.src = 'asset/lars.png';
+  if (event.key == '2') image.src = 'asset/levi.png';
+  if (event.key == '3') image.src = 'asset/lenny.png';
 });
 
 document.addEventListener('keyup', (event) => {
